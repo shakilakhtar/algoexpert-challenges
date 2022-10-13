8f37804ea56f97a2a58da9b5bc27e5d76f61e21e
Shakil <2shakil.akhtar@gmail.com>
2022-10-13 06:18:16 +0530

    added new solution

---
+++ src/com/challenge/algoexpert/binarytrees/FindNodesDistanceK.java
@@ -0,0 +1,10 @@
+package com.challenge.algoexpert.binarytrees;
+
+import java.util.ArrayList;
+
+public class FindNodesDistanceK {
+    public ArrayList<Integer> findNodesDistanceK(BinaryTree tree, int target, int k) {
+        // Write your code here.
+        return new ArrayList<Integer>();
+    }
+}
